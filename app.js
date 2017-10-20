4cade49e4ecbf8cb437f7d45597075c1da6882c7
Donde Lapera <dondelapera@Dondes-MacBook-Pro.local>
2017-10-20 12:13:53 +0800

    adding .env file

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -34,6 +37,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
